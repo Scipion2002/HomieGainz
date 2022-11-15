5af5c4f901eb502fa4c93d54a12d9485441ebac8
Scipion2002 <79728249+Scipion2002@users.noreply.github.com>
2022-11-14 20:11:57 -0700

    Forgot that I can do this on Android Studio. Login Page is pretty much done, needs connections and page transitions which I'm working on it right now

---
+++ homiegainz_front_end/lib/Login/login_page.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController _usernameController = TextEditingController();
+    TextEditingController _passwordController = TextEditingController();
+
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.only(top: 25),
+              child: const Image(
+                  image: AssetImage("assets/images/HomieGainzImg.png")),
+            ),
+            const Center(
+                heightFactor: 2,
+                child: Text(
+                  "Login",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                )),
+            Container(
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 15, vertical: 25),
+                child: TextField(
+                  controller: _usernameController,
+                  decoration: const InputDecoration(
+                      prefixIcon: Icon(Icons.account_circle_outlined,
+                          color: Colors.grey),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.green),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.blue)),
+                      hintText: 'Enter Username...',
+                      labelText: 'Enter Username',
+                      labelStyle: TextStyle(color: Colors.grey),
+                      focusColor: Colors.green),
+                )),
+            Container(
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 15, vertical: 25),
+                child: TextField(
+                  controller: _passwordController,
+                  decoration: const InputDecoration(
+                      prefixIcon: Icon(Icons.account_circle_outlined,
+                          color: Colors.grey),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.green),
+                      ),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.blue)),
+                      hintText: 'Enter Password...',
+                      labelText: 'Enter Password',
+                      labelStyle: TextStyle(color: Colors.grey),
+                      focusColor: Colors.green),
+                )),
+            Container(
+                margin: const EdgeInsets.only(top: 25, bottom: 25),
+                child: Row(
+                  children: [
+                    Expanded(
+                        flex: 1,
+                        child: Container(
+                            alignment: Alignment.center,
+                            child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.blue, // background
+                                ),
+                                onPressed: () {
+
+                                },
+                                child: const Text('Log In',
+                                    style: TextStyle(color: Colors.black))))),
+                    Expanded(
+                        flex: 1,
+                        child: Container(
+                            alignment: Alignment.center,
+                            child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.blue, // background
+                                ),
+                                onPressed: () {
+
+                                },
+                                child: const Text('Sign Up',
+                                    style: TextStyle(color: Colors.black)))))
+                  ],
+                )),
+          ],
+        ),
+      ),
+    ));
+  }
+}
