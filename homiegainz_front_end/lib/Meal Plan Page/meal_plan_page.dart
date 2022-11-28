48535053fce2b63922f9826d2d9befafbdc24d55
Scipion2002 <79728249+Scipion2002@users.noreply.github.com>
2022-11-28 01:01:09 -0700

    Fixed backend, Meal Plan Page done!
    Eureka was being eureka, got it fixed and now the all the meals are being displayed

--- homiegainz_front_end/lib/Meal Plan Page/meal_plan_page.dart
+++ homiegainz_front_end/lib/Meal Plan Page/meal_plan_page.dart
@@ -1,29 +1,67 @@
-import 'package:flutter/material.dart';
+import 'dart:collection';
+import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import '../../util/globals.dart' as globals;
+import '../util/requests.dart';
 import 'Front/meal_card.dart';
 
-class MealPlanPage extends StatelessWidget {
+class MealPlanPage extends StatefulWidget {
   const MealPlanPage({Key? key}) : super(key: key);
 
   @override
+  State<MealPlanPage> createState() => _MealPlanPageState();
+}
+
+class _MealPlanPageState extends State<MealPlanPage> {
+  Requests requests = Requests();
+  List<MealCard> userMeals = [];
+
+  @override
   Widget build(BuildContext context) {
+
+    Future<String> getUserMeals = requests.makeGetRequestWithAuth(
+        "http://10.0.2.2:9000/mealPlans/${globals.mealPlanID}",
+        globals.username,
+        globals.password);
+
     return SafeArea(
       child: Scaffold(
-          body: Column(
-            children: [
-              const Text("Your Meal Plan",
-                  style:
-                  TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
-              MealCard(
-                mealID: 1,
-                mealName: "salad",
-                description:
-                "This meal is meant for you to get nutritious stuff on your belly!",
-                ingredients: "Lettuce, Tomato, Carrot, Boiled Egg",
-                directions: "chop up the veggies and put them together, do this while you wait for the egg to boil",
-              ),
-            ],
-          )),
+          body: SingleChildScrollView(child: FutureBuilder<String>(
+            future: getUserMeals,
+            builder: (context, snapshot){
+              if(snapshot.hasData){
+                userMeals.clear();
+
+                LinkedHashMap<String, dynamic> mealInformation = json.decode(snapshot.data!);
+                print(mealInformation);
+
+                List<dynamic> meals = mealInformation["meals"];
+
+                for (var meal in meals){
+                  userMeals.add(MealCard(
+                    mealID: meal["id"],
+                    mealName: meal["name"],
+                    description: meal["description"],
+                    ingredients: meal["ingredientList"],
+                    directions: meal["directions"],
+                    isAdded: true,
+                  ));
+                }
+                return Column(children: userMeals);
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+              return Center(
+                  heightFactor: 20,
+                  child: Container(
+                    alignment: Alignment.center,
+                    child: const CircularProgressIndicator(
+                      color: Colors.tealAccent,
+                    ),
+                  ));
+            },
+          )) ),
     );
   }
 }
